da8df4d67ad70ecbaf3561dcce75aa4e502116f5
SeongHwan1001 <limmli04210@gmail.com>
2019-07-24 10:08:03 +0900

    Revert "2019.07.23(화) 5조(지윤, 성환, 자겸, 지은) 채팅 프로그램 GUI 최종본"
    This reverts commit 3ec1849349e346184f983c84560ebd3587dfe20a.

--- chatClient_end/src/chatClient/Main.java
+++
@@ -1,9 +0,0 @@
-package chatClient;
-
-public class Main {
-	public static void main(String[] args) {
-		ClientChatService c = new ChatClient();
-		ChatGUI gui = new ChatGUI(c);
-		c.setClientChatListener(gui);
-	}
-}
