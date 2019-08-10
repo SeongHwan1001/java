d52ab355efe43ec8d601a5779a252c2578b4a832
SeongHwan1001 <limmli04210@gmail.com>
2019-08-10 14:50:43 +0900

    2019.08.08(금) 5조 DataBase CRUD

---
+++ DB-CRUD2/src/dbcrud2/Data.java
@@ -0,0 +1,31 @@
+package dbcrud2;
+
+public class Data {
+	String name;
+	int age;
+
+	public Data() {
+	}
+
+	public Data(String name, int age) {
+		this.age = age;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ DB-CRUD2/src/dbcrud2/Main.java
@@ -0,0 +1,7 @@
+package dbcrud2;
+
+public class Main {
+	public static void main(String[] args) {
+		CRUDGUI gui = new CRUDGUI();
+	}
+}
